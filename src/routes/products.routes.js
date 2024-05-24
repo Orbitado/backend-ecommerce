f2c94dfd153b7ff2fb5251dae18246c827a41430
Orbitadoo <juanpaavaldezz@gmail.com>
2024-05-24 11:34:23 -0300

    [First Solution]

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,14 +2,15 @@ import { Router } from "express";
 import fs from "fs"
 
 const router = Router();
-// La ruta raíz get deberá listar todos los productos de la base de datos.
 
 const products = JSON.parse(fs.readFileSync('./data/products.json', 'utf-8'));
 
+// La ruta raíz get deberá listar todos los productos de la base de datos.
 router.get('/', (req, res) => {
     res.json(products);
 })
 
+// La ruta GET /:pid deberá traer sólo el producto con el id proporcionado
 router.get('/:pid', async (req, res) => {
     const { pid } = req.params;
     const product = await products.find(product => product.id == pid);
@@ -20,5 +21,31 @@ router.get('/:pid', async (req, res) => {
         res.json(product);
     }
 })
+
+// La ruta raíz POST / deberá agregar un nuevo producto a la base de datos.
+router.post('/', (req, res) => {
+    const { title, description, code, price, stock, category } = req.body;
+    const newId = products[products.lenght - 1].id + 1;
+
+    if(!title || !description || !code || !price || !stock || !category) {
+        return res.status(400).json({ error: 'Todos los campos son obligatorios' })
+    } else {
+            const newProduct = {
+                id: newId,
+                title,
+                description,
+                code,
+                price,
+                status: true,
+                stock: products.stock + stock,
+                category
+            }
+
+            products.push(newProduct);
+            fs.writeFileSync('./data/products.json', JSON.stringify(products, null, '\t'));
+        }
+        res.json(products); 
+    } 
+)
 
 export default router
