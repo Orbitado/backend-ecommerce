f23bc3850bcf2caabe6aa8d386914fc3af1a0f63
Orbitadoo <juanpaavaldezz@gmail.com>
2024-06-05 13:49:19 -0300

    adding-hbs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-import Express from "express"
-import productsRoutes from "./routes/products.routes.js"
-import cartRoutes from "./routes/carts.routes.js"
+import Express from "express";
+import productsRoutes from "./routes/products.routes.js";
+import cartRoutes from "./routes/carts.routes.js";
+import handlebars from "express-handlebars";
+import viewsRoutes from "./routes/views.routes.js";
+import __dirname from "./dirname.js";
 
 const app = Express();
 
@@ -10,8 +13,23 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
-})
+  console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
 
-app.use('/api/products', productsRoutes);
-app.use('/api/carts', cartRoutes);
+// Configuracion de handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+app.use("/", viewsRoutes);
+// Fin de la configuración de handlebars
+
+app.use("/api/products", productsRoutes);
+app.use("/api/carts", cartRoutes);
