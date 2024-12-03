e88e70b38a6f4bbf619c0abd51fd3ee21232a0b6
Orbitadoo <juanpaavaldezz@gmail.com>
2024-12-03 08:55:06 -0300

    feat: jwt-util

--- src/utils/token.util.js
+++ src/utils/token.util.js
@@ -1,6 +1,28 @@
 import jwt from "jsonwebtoken";
 
+const { JWT_SECRET } = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
 function generateToken(user) {
-  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1d" });
+  const token = jwt.sign(user, JWT_SECRET, {
+    expiresIn: 60 * 60 * 24 * 7,
+  });
   return token;
 }
+
+function verifyToken(token) {
+  const isValidToken = jwt.verify(token, JWT_SECRET);
+  return isValidToken;
+}
+
+function finishToken(token) {
+  const token = jwt.sign(user, JWT_SECRET, {
+    expiresIn: 1,
+  });
+  return token;
+}
+
+export { generateToken, verifyToken, finishToken };
