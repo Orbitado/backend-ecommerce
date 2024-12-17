07473741eb0af520f2ca36f2dc6a956966789773
Orbitadoo <juanpaavaldezz@gmail.com>
2024-12-17 10:01:28 -0300

    feat: added product controller and refactored api

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,96 @@
+import { productDBManager } from "../managers/product.manager.js";
+import { uploader } from "../utils/multer.js";
+
+const ProductService = new productDBManager();
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const result = await ProductService.getAllProducts(req.query);
+    res.send({
+      status: "success",
+      payload: result,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export const getProductByID = async (req, res) => {
+  try {
+    const result = await ProductService.getProductByID(req.params.pid);
+    res.send({
+      status: "success",
+      payload: result,
+    });
+  } catch (error) {
+    res.status(400).send({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export const createProduct = [
+  uploader.array("thumbnails", 3),
+  async (req, res) => {
+    if (req.files) {
+      req.body.thumbnails = req.files.map((file) => file.path);
+    }
+
+    try {
+      const result = await ProductService.createProduct(req.body);
+      res.send({
+        status: "success",
+        payload: result,
+      });
+    } catch (error) {
+      res.status(400).send({
+        status: "error",
+        message: error.message,
+      });
+    }
+  },
+];
+
+export const updateProduct = [
+  uploader.array("thumbnails", 3),
+  async (req, res) => {
+    if (req.files) {
+      req.body.thumbnails = req.files.map((file) => file.filename);
+    }
+
+    try {
+      const result = await ProductService.updateProduct(
+        req.params.pid,
+        req.body
+      );
+      res.send({
+        status: "success",
+        payload: result,
+      });
+    } catch (error) {
+      res.status(400).send({
+        status: "error",
+        message: error.message,
+      });
+    }
+  },
+];
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const result = await ProductService.deleteProduct(req.params.pid);
+    res.send({
+      status: "success",
+      payload: result,
+    });
+  } catch (error) {
+    res.status(400).send({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
