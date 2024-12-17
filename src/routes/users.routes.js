c2cb774a47d016ea46f5162fd3d51f57fbf55e91
Orbitadoo <juanpaavaldezz@gmail.com>
2024-12-17 10:07:26 -0300

    feat: added user controller and refactored api

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,71 +1,18 @@
 import { Router } from "express";
-import { userDBManager } from "../managers/user.manager.js";
+import {
+  getAllUsers,
+  getUserByID,
+  createUser,
+  updateUserByID,
+  deleteUser,
+} from "../controllers/user.controller.js";
 
 const router = Router();
-const UserService = new userDBManager();
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await UserService.getAllUsers();
-    res.status(200).send(users);
-  } catch (error) {
-    res.status(400).send({
-      status: "error",
-      message: error.message,
-    });
-  }
-});
-
-router.get("/:uid", async (req, res) => {
-  try {
-    const user = await UserService.getUserByID(req.params.uid);
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(400).send({
-      status: "error",
-      message: error.message,
-    });
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const user = await UserService.createUser(req.body);
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send({
-      status: "error",
-      message: error.message,
-    });
-  }
-});
-
-router.put("/:pid", async (req, res) => {
-  const { pid } = req.params;
-  const userUpdate = req.body;
-
-  try {
-    const result = await UserService.updateUserByID(pid, userUpdate);
-    res.status(200).json({
-      status: "success",
-      message: "Usuario actualizado correctamente",
-      result,
-    });
-  } catch (error) {
-    res.status(500).json({ status: "error", message: error.message });
-  }
-});
-
-router.delete("/:uid", async (req, res) => {
-  try {
-    const user = await UserService.deleteUser(req.params.uid);
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(400).send({
-      status: "error",
-      message: error.message,
-    });
-  }
-});
+router.get("/", getAllUsers);
+router.get("/:uid", getUserByID);
+router.post("/", createUser);
+router.put("/:pid", updateUserByID);
+router.delete("/:uid", deleteUser);
 
 export default router;
